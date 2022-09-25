4095a42a9606dd7bf5874194bde3459360507718
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2022-09-25 20:21:35 +0530

    Initial commit with features by Sandhu-Sahil

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var timer = 0
+setInterval(()=> {timer = timer + 1;}, 1000);
+console.log(timer)
+
+const redirect = ()=>{
+    window.location.replace("https://gdscjiit128.tech/#/");
+}
+
+setTimeout(()=>{
+    redirect()
+},5633)
