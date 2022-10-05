a913ff927a8025d8299f7075ee0b005f3e8ef876
Sarthak Pant <67951999+sarthakpant772@users.noreply.github.com>
2022-10-05 22:53:34 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ console.log(timer)
 
 const redirect = () => {
   window.location.replace(
-    'https://gdsc.community.dev/e/m5dzbq/',
+    'https://gdsc.community.dev/events/details/developer-student-clubs-jaypee-institute-of-information-technology-sector-128-noida-presents-compose-camp-session-1-history-of-android-development-kotlin-fundamentals/',
   )
 }
 
