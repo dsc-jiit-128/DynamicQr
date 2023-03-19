cffa36e24c5ddc9a53e57a9f3ca042aedc3240da
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-03-19 11:03:59 +0530

    Added dynamic redirection

--- javascript.js
+++ javascript.js
@@ -5,9 +5,48 @@ setInterval(() => {
 console.log(timer)
 
 const redirect = () => {
-  window.location.replace(
-    'https://gdsc.community.dev/e/m6ctmp/',
-  )
+  console.log(window.location.search);
+
+  if (window.location.search == "/DynamicQr/"){
+
+    window.location.replace(
+      'https://gdsc.community.dev/e/m6ctmp/',
+    )
+    return;
+
+  } else if (window.location.search == "/DynamicQr/sahil"){
+
+    window.location.replace(
+      'https://github.com/Sandhu-Sahil',
+    )
+    return;
+
+  } else if (window.location.search == "/DynamicQr/vishesh"){
+
+    window.location.replace(
+      'https://gdsc.community.dev/e/m6ctmp/',
+    )
+    return;
+
+  } else if (window.location.search == "/DynamicQr/tanay"){
+
+    window.location.replace(
+      'https://gdsc.community.dev/e/m6ctmp/',
+    )
+    return;
+
+  } else if (window.location.search == "/DynamicQr/sanat"){
+
+    window.location.replace(
+      'https://gdsc.community.dev/e/m6ctmp/',
+    )
+    return;
+
+  }
+
+  // window.location.replace(
+  //   'https://gdsc.community.dev/e/m6ctmp/',
+  // )
 }
 
 setTimeout(() => {
