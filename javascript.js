50b75ae02c913a57d7c86e97338ac732b87a1573
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2024-09-26 18:28:29 +0530

    Refactor redirect logic using array of objects for parameters and links

--- javascript.js
+++ javascript.js
@@ -1,75 +1,43 @@
-var timer = 0
+var timer = 0;
 setInterval(() => {
-  timer = timer + 1
-}, 1000)
-console.log(timer)
+  timer += 1;
+}, 1000);
+
+// set default link here:
+const defaultLink = "https://gdg.community.dev/e/m6jj8r/"
+
+const redirectLinks = [
+  { param: "?hunt=sahil", link: "https://github.com/Sandhu-Sahil" },
+  { param: "?hunt=vishesh", link: "https://github.com/entropyconquers" },
+  { param: "?hunt=tanay", link: "https://github.com/TanayK07" },
+  { param: "?hunt=sanat", link: "https://instagram.com/sanatbhatia?igshid=YmMyMTA2M2Y=" },
+  { param: "?hunt=entry", link: "https://gdsc-jiit128.tech/bitbox-validation" },
+  { param: "?hunt=CrypticHunt", link: "https://cypher-dash.netlify.app/" },
+  { param: "?hunt=CipherDash", link: "https://cipher-dash.netlify.app/" },
+  { param: "?hunt=vaibhav", link: "https://github.com/VaibhavKatariya" },
+];
 
 const redirect = () => {
-  console.log(window.location.search);
 
-  if (window.location.search == ""){
-
-    window.location.replace(
-      'https://gdg.community.dev/e/m6jj8r/',
-    )
+  if (window.location.search === "") {
+    window.location.replace(defaultLink);
     return;
-
-  } else if (window.location.search == "?hunt=sahil"){
-
-    window.location.replace(
-      'https://github.com/Sandhu-Sahil',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=vishesh"){
-
-    window.location.replace(
-      'https://github.com/entropyconquers',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=tanay"){
-
-    window.location.replace(
-      'https://github.com/TanayK07',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=sanat"){
-
-    window.location.replace(
-      'https://instagram.com/sanatbhatia?igshid=YmMyMTA2M2Y=',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=entry"){
-
-    window.location.replace(
-      'https://gdsc-jiit128.tech/bitbox-validation',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=CrypticHunt"){
-
-    window.location.replace(
-      'https://cypher-dash.netlify.app/',
-    )
-    return;
-
-  } else if (window.location.search == "?hunt=CipherDash"){
-
-    window.location.replace(
-      'https://cipher-dash.netlify.app/',
-    )
-    return;
-
   }
 
-  // window.location.replace(
-  //   'https://gdsc.community.dev/e/m6ctmp/',
-  // ) 
-}
+  const foundLink = redirectLinks.find(
+    (item) => item.param === window.location.search
+  );
+
+  if (foundLink) {
+    window.location.replace(foundLink.link);
+    return;
+  }
+
+  // If no param match found go to default link
+  window.location.replace(defaultLink);
+
+};
 
 setTimeout(() => {
-  redirect()
-}, 5633)
+  redirect();
+}, 5633);
